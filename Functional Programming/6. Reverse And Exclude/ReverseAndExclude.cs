ef09fa4cf5223968fd67b8b1af59cfac97bfc65b
Radoslav Ivanov <100360507+RadoslavI@users.noreply.github.com>
2022-06-28 14:45:22 +0300

    Update ReverseAndExclude.cs

--- Functional Programming/6. Reverse And Exclude/ReverseAndExclude.cs
+++ Functional Programming/6. Reverse And Exclude/ReverseAndExclude.cs
@@ -6,7 +6,7 @@ public class ReverseAndExclude
 {
     static void Main(string[] args)
     {
-        Console.WriteLine(30%3);
+        //Console.WriteLine(30%3);
         List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
         int n = int.Parse(Console.ReadLine());
         List<int> numbersToPrint = new List<int>();
