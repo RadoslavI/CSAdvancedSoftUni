37daa3cc9b62f9b000a93b60f0d8f7e64d941d98
Radoslav Ivanov <radogi001@gmail.com>
2022-07-01 16:21:26 +0300

    commit

---
+++ Iterators and Comparators - Exercise/7-Custom-Comparator/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _7_Custom_Comparator
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int, int, int> sortFunction = (x, y)
+                => (x % 2 == 0 && y % 2 != 0) ? -1 : 
+                (x % 2 != 0 && y % 2 == 0) ? 1 : 
+                x > y ? 1 : x < y ? -1 : 0;
+
+            Array.Sort(nums, (x, y) => sortFunction(x, y));
+            Console.WriteLine(String.Join(" ", nums));
+        }
+    }
+}
