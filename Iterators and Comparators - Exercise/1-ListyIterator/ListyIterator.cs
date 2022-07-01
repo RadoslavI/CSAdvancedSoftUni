4032f0ecfce2de3406d73afb927c73be990e451b
Radoslav Ivanov <radogi001@gmail.com>
2022-07-01 13:42:30 +0300

    commit

---
+++ Iterators and Comparators - Exercise/1-ListyIterator/ListyIterator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> collection;
+        private int currIndex;
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currIndex = 0;
+        }   
+
+        public bool hasNext() => currIndex < collection.Count - 1;
+
+        public bool Move()
+        {
+            bool canMove = hasNext();
+            if (canMove)
+            {
+                currIndex++;
+            }
+            return canMove;
+        }
+
+        public void Print()
+        {
+            if(collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine($"{collection[currIndex]}");
+        } 
+    }
+}
---
+++ Iterators and Comparators - Exercise/1-ListyIterator/StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _1_ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listy = null;
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var commands = input.Split();
+
+                if (commands[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(commands.Skip(1).ToArray());
+                }
+                else if (commands[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (commands[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (commands[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.hasNext());
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+
+        }
+    }
+}
