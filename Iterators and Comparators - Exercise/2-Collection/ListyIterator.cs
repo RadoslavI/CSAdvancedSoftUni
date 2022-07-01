0c455c60b80d803c71d1feee03dae6310d848542
Radoslav Ivanov <radogi001@gmail.com>
2022-07-01 13:43:58 +0300

    initial commit

---
+++ Iterators and Comparators - Exercise/2-Collection/ListyIterator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Collection
+{
+    public class ListyIterator<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+        private int currIndex;
+        public ListyIterator(params T[] data)
+        {
+            collection = new List<T>(data);
+            currIndex = 0;
+        }
+
+
+
+        public bool hasNext() => currIndex < collection.Count - 1;
+
+        public bool Move()
+        {
+            bool canMove = hasNext();
+            if (canMove)
+            {
+                currIndex++;
+            }
+            return canMove;
+        }
+
+        public void Print()
+        {
+            if (collection.Count == 0)
+            {
+                throw new ArgumentException("Invalid Operation!");
+            }
+
+            Console.WriteLine($"{collection[currIndex]}");
+        }
+
+        public void PrintAll() => Console.WriteLine(String.Join(" ", collection));
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T element in collection)
+            {
+                yield return element;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Iterators and Comparators - Exercise/2-Collection/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _2_Collection
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> listy = null;
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                var commands = input.Split();
+
+                if (commands[0] == "Create")
+                {
+                    listy = new ListyIterator<string>(commands.Skip(1).ToArray());
+                }
+                else if (commands[0] == "Move")
+                {
+                    Console.WriteLine(listy.Move());
+                }
+                else if (commands[0] == "Print")
+                {
+                    listy.Print();
+                }
+                else if (commands[0] == "HasNext")
+                {
+                    Console.WriteLine(listy.hasNext());
+                }
+                else if (commands[0] == "PrintAll")
+                {
+                    listy.PrintAll();
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
