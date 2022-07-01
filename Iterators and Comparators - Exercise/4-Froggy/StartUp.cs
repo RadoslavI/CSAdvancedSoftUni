4eccf29565dfacb97facfa21530de18558782835
Radoslav Ivanov <radogi001@gmail.com>
2022-07-01 14:41:20 +0300

    commit

---
+++ Iterators and Comparators - Exercise/4-Froggy/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _4_Froggy
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var lake = new Lake(Console.ReadLine().Split(", ").Select(int.Parse).ToArray());
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
