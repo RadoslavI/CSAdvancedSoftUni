19d53f57e47a365b037cfd1e37ff9fb7e7ef38cb
Radoslav Ivanov <radogi001@gmail.com>
2022-07-01 16:07:50 +0300

    commit

---
+++ Iterators and Comparators - Exercise/6-EqualityLogic/Person.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_EqualityLogic
+{
+    public class Person :IComparable<Person>
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public int CompareTo(Person other)
+        {
+            int result = Name.CompareTo(other.Name);
+            if (result==0)
+                result = Age.CompareTo(other.Age);
+            return result;
+        }
+
+        public override int GetHashCode() => Name.GetHashCode() ^ Age.GetHashCode();
+
+        public override bool Equals(object? obj)
+        {
+            var other = obj as Person;
+            if (other == null) return false;
+            return Age == other.Age && Name == other.Name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+
+
+    }
+}
---
+++ Iterators and Comparators - Exercise/6-EqualityLogic/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6_EqualityLogic
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var sortedSet = new SortedSet<Person>();
+            var hashSet = new HashSet<Person>();
+            var n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split();
+                var person = new Person(tokens[0], int.Parse(tokens[1]));
+                sortedSet.Add(person);
+                hashSet.Add(person);
+            }
+
+            Console.WriteLine(sortedSet.Count);
+            Console.WriteLine(hashSet.Count);
+
+        }
+    }
+}
