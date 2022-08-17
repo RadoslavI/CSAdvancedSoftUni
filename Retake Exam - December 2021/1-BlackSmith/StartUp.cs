ce285ea0b233840e37d5b408209c247134d4a35c
Radoslav Ivanov <radogi001@gmail.com>
2022-08-17 22:41:24 +0300

    initial commit

---
+++ Retake Exam - December 2021/1-BlackSmith/StartUp.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1_BlackSmith
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] steel = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int[] carbon = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queueSteel = new Queue<int>(steel);
+            Stack<int> stackCarbon = new Stack<int>(carbon);
+
+            //sword name => count
+            SortedDictionary<string, int> swords = new SortedDictionary<string, int>()
+            {
+                {"Broadsword", 0},
+                {"Sabre", 0},
+                {"Katana", 0},
+                {"Shamshir", 0},
+                {"Gladius", 0}
+            };
+
+            int countSwords = 0;
+            while (queueSteel.Count > 0 && stackCarbon.Count > 0)
+            {
+                int currSteel = queueSteel.Peek();
+                int currCarbon = stackCarbon.Peek();
+                int sum = currCarbon + currSteel;
+
+                if (sum == 70)
+                {
+                    swords["Gladius"]++;
+                    queueSteel.Dequeue();
+                    stackCarbon.Pop();
+                    countSwords++;
+                }
+                else if (sum == 80)
+                {
+                    swords["Shamshir"]++;
+                    queueSteel.Dequeue();
+                    stackCarbon.Pop();
+                    countSwords++;
+                }
+                else if (sum == 90)
+                {
+                    swords["Katana"]++;
+                    queueSteel.Dequeue();
+                    stackCarbon.Pop();
+                    countSwords++;
+                }
+                else if (sum == 110)
+                {
+                    swords["Sabre"]++;
+                    queueSteel.Dequeue();
+                    stackCarbon.Pop();
+                    countSwords++;
+                }
+                else if (sum == 150)
+                {
+                    swords["Broadsword"]++;
+                    queueSteel.Dequeue();
+                    stackCarbon.Pop();
+                    countSwords++;
+                }
+                else
+                {
+                    queueSteel.Dequeue();
+                    stackCarbon.Pop();
+                    stackCarbon.Push(currCarbon + 5);
+                }
+            }
+
+            if (countSwords > 0)
+            {
+                Console.WriteLine($"You have forged {countSwords} swords.");
+            }
+            else
+            {
+                Console.WriteLine("You did not have enough resources to forge a sword.");
+            }
+
+            if (queueSteel.Count == 0)
+            {
+                Console.WriteLine("Steel left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Steel left: {String.Join(", ", queueSteel.Select(x => x.ToString()))}");
+            }
+
+            if (stackCarbon.Count == 0)
+            {
+                Console.WriteLine("Carbon left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Carbon left: {String.Join(", ", stackCarbon.Select(x => x.ToString()))}");
+            }
+
+
+            foreach (var item in swords)
+            {
+                if (item.Value > 0)
+                {
+                    Console.WriteLine($"{item.Key}: {item.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ Retake Exam - December 2021/2-Armory/StartUp.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Drawing;
+
+namespace TruffleHunter
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n,n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] currRow = Console.ReadLine().Replace(" ", string.Empty).ToCharArray();
+                for (int i = 0; i < n; i++)
+                {
+                    matrix[row, i] = currRow[i];
+                }
+            }
+
+            int countBlack = 0;
+            int countWhite = 0;
+            int countSummer = 0;
+            int eatenCount = 0;
+
+            string command = Console.ReadLine();
+
+            while (command != "Stop the hunt")
+            {
+                string commandName = command.Split()[0];
+                int row = int.Parse(command.Split()[1]);
+                int col = int.Parse(command.Split()[2]);
+
+                if (commandName == "Collect")
+                {
+                    char truffel = matrix[row, col];
+
+                    matrix[row, col] = '-';
+
+                    switch (truffel)
+                    {
+                        case 'B':
+                            countBlack++;
+                            break;
+                        case 'W':
+                            countWhite++;
+                            break;
+                        case 'S':
+                            countSummer++;
+                            break;
+                    }
+
+                }
+                else if (commandName == "Wild_Boar")
+                {
+                    string direction = command.Split()[3];
+
+                    switch (direction)
+                    {
+                        case "up":
+                            while (isValidRow(row, n))
+                            {
+                                if (BoarEat(row, col, matrix))
+                                {
+                                    eatenCount++;
+                                }
+
+                                row -= 2;
+                            }
+                            break;
+                        case "down":
+                            while (isValidRow(row, n))
+                            {
+                                if (BoarEat(row, col, matrix))
+                                {
+                                    eatenCount++;
+                                }
+
+                                row += 2;
+                            }
+                            break;
+                        case "left":
+                            while (isValidCol(col, n))
+                            {
+                                if (BoarEat(row, col, matrix))
+                                {
+                                    eatenCount++;
+                                }
+
+                                col -= 2;
+                            }
+                            break;
+                        case "right":
+                            while (isValidCol(col, n))
+                            {
+                                if (BoarEat(row, col, matrix))
+                                {
+                                    eatenCount++;
+                                }
+
+                                col += 2;
+                            }
+                            break;
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Peter manages to harvest {countBlack} black, {countSummer} summer, and {countWhite} white truffles.");
+            Console.WriteLine($"The wild boar has eaten {eatenCount} truffles.");
+
+            PrintMatrix(matrix);
+        }
+
+        private static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool BoarEat(int row, int col, char[,] matrix)
+        {
+            var currSymbol = matrix[row, col];
+            if (currSymbol == 'S' || currSymbol == 'B' || currSymbol == 'W')
+            {
+                matrix[row, col] = '-';
+                return true;
+            }
+            return false;
+        }
+
+        public static bool isValidRow(int row, int size)
+        {
+            return row >= 0 && row < size;
+        }
+        public static bool isValidCol(int col, int size)
+        {
+            return col >= 0 && col < size;
+        }
+    }
+}
